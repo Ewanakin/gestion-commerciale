f6ec0b10573c35396cb3fd92ff2879dfb8da2c89
unknown <daca.loris@gmail.com>
2022-11-14 18:21:12 +0100

    GestionUtilisateur BLL

---
+++ CommercialBLL/GestionUtilisateurs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using CommercialBO;
+using CommercialDAL;
+
+namespace CommercialBLL
+{
+    public class GestionUtilisateurs
+    {
+        private static GestionUtilisateurs uneGestionUtilisateurs; // objet BLL
+
+        // Accesseur en lecture
+        public static GestionUtilisateurs GetGestionUtilisateurs()
+        {
+            if (uneGestionUtilisateurs == null)
+            {
+                uneGestionUtilisateurs = new GestionUtilisateurs();
+            }
+            return uneGestionUtilisateurs;
+        }
+        public static int CreerUtilisateur(Utilisateur ut)
+        {
+            return UtilisateurDAO.CheckUtilisateur(ut);
+        }
+
+    }
+}
