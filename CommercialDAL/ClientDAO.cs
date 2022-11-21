a330942fbb55538f5baf6b5193522035e693a96f
unknown <daca.loris@gmail.com>
2022-11-21 21:17:03 +0100

    fix DAL CLIENT

--- CommercialDAL/ClientDAO.cs
+++ CommercialDAL/ClientDAO.cs
@@ -5,7 +5,6 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CommercialBO;
 
 namespace CommercialDAL
 {
@@ -27,7 +26,7 @@ namespace CommercialDAL
             string villeAdresseLiv;
             string villeAdresseFac;
             Client unClient;
-            
+
             // Connexion à la BD
             SqlConnection maConnexion = ConnexionBD.GetConnexionBD().GetSqlConnexion();
             // Création d'une liste vide d'objets Produits
@@ -40,24 +39,24 @@ namespace CommercialDAL
             while (monReader.Read())
             {
                 code = Int32.Parse(monReader["code_cli"].ToString());
-                nom = monReader["nom_cli"].ToString();
+                nom = monReader["num_cli"].ToString();
                 numeroTel = Int32.Parse(monReader["num_tel_cli"].ToString());
                 numeroFax = Int32.Parse(monReader["num_fax_cli"].ToString());
-                email = monReader["email"].ToString();
+                email = monReader["adresse_mail_cli"].ToString();
                 numAdresseLiv = Int32.Parse(monReader["num_adresse_fact_cli"].ToString());
                 numAdresseFac = Int32.Parse(monReader["num_adresse_livr_cli"].ToString());
                 rueAdresseLiv = monReader["rue_adresse_liv_cli"].ToString();
                 rueAdresseFac = monReader["rue_adresse_fact_cli"].ToString();
                 codePostalAdresseLiv = Int32.Parse(monReader["code_postal_fact_cli"].ToString());
                 codePostalAdresseFac = Int32.Parse(monReader["code_postal_livr_cli"].ToString());
-                villeAdresseLiv = monReader["ville_adresse_liv_cli"].ToString();
+                villeAdresseLiv = monReader["ville_adresse_livr_cli"].ToString();
                 villeAdresseFac = monReader["ville_adresse_fact_cli"].ToString();
                 unClient = new Client(code, nom, numeroTel, numeroFax, email, numAdresseLiv, numAdresseFac, rueAdresseLiv, rueAdresseFac, codePostalAdresseLiv, codePostalAdresseFac, villeAdresseLiv, villeAdresseFac);
                 lesClients.Add(unClient);
             }
             // Fermeture de la connexion
             maConnexion.Close();
-            
+
             return lesClients;
         }
     }
