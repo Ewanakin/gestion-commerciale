2cee3867dbca53d9e2ee5cf34a100363eadd0bf1
Ewan Quioc <quioc.ewan@gmail.com>
2022-12-08 19:42:41 +0100

    recup modif

--- CommercialGUI/FrmDevis.Designer.cs
+++ CommercialGUI/FrmDevis.Designer.cs
@@ -412,7 +412,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1417, 675);
+            this.ClientSize = new System.Drawing.Size(1688, 855);
             this.Controls.Add(this.gpDevis);
             this.Controls.Add(this.btnNewDevis);
             this.Controls.Add(this.dtgDevis);
