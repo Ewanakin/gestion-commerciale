544bbb7c5c9b71555c08a6c8274a0a15f639f03e
unknown <daca.loris@gmail.com>
2022-12-15 10:09:29 +0100

    front sprint 4

---
+++ CommercialGUI/FrmViewSummary.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CommercialGUI
+{
+    partial class FrmViewSummary
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvSummary = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSummary)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvSummary
+            // 
+            this.dgvSummary.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSummary.Location = new System.Drawing.Point(204, 308);
+            this.dgvSummary.Name = "dgvSummary";
+            this.dgvSummary.RowHeadersWidth = 82;
+            this.dgvSummary.RowTemplate.Height = 33;
+            this.dgvSummary.Size = new System.Drawing.Size(1861, 1043);
+            this.dgvSummary.TabIndex = 0;
+            this.dgvSummary.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvSummary_CellContentClick);
+            // 
+            // FrmViewSummary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2261, 1522);
+            this.Controls.Add(this.dgvSummary);
+            this.Name = "FrmViewSummary";
+            this.Text = "FrmViewSummary";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSummary)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvSummary;
+    }
+}
---
+++ CommercialGUI/FrmViewSummary.cs
@@ -0,0 +1,75 @@
+﻿using CommercialBLL;
+using CommercialBO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommercialGUI
+{
+    public partial class FrmViewSummary : Form
+    {
+        public FrmViewSummary()
+        {
+            InitializeComponent();
+            // Blocage de la génération automatique des colonnes
+            dgvSummary.AutoGenerateColumns = false;
+
+            // Récupération de chaîne de connexion à la BD à l'ouverture du formulaire
+            GestionProduits.SetchaineConnexion(ConfigurationManager.ConnectionStrings["gestion-commerciale"]);
+            // Création d'une en-tête de colonne pour la colonne 1
+            DataGridViewTextBoxColumn nomCliColumn = new DataGridViewTextBoxColumn();
+            nomCliColumn.Name = "Nom Client";
+            nomCliColumn.DataPropertyName = "ClientNom";
+            nomCliColumn.HeaderText = "Nom Client";
+            // Création d'une en-tête de colonne pour la colonne 2
+            DataGridViewTextBoxColumn nbDevisColumn = new DataGridViewTextBoxColumn();
+            nbDevisColumn.Name = "nombreDevis";
+            nbDevisColumn.DataPropertyName = "nombreDevis";
+            nbDevisColumn.HeaderText = "Nombre Devis";
+            // Création d'une en-tête de colonne pour la colonne 3
+            DataGridViewTextBoxColumn nbDevisAcceptésColumn = new DataGridViewTextBoxColumn();
+            nbDevisAcceptésColumn.Name = "Nombre Devis Accepté";
+            nbDevisAcceptésColumn.DataPropertyName = "nbDevisAccepté";
+            nbDevisAcceptésColumn.HeaderText = "Nombre Devis Accepté";
+            // Création d'une en-tête de colonne pour la colonne 4
+            DataGridViewTextBoxColumn pourcentageAcceptéColumn = new DataGridViewTextBoxColumn();
+            pourcentageAcceptéColumn.Name = "Pourcentage Devis Accepté";
+            pourcentageAcceptéColumn.DataPropertyName = "pourcentageAccepté";
+            pourcentageAcceptéColumn.HeaderText = "Pourcentage Devis Accepté";
+            // Création d'une en-tête de colonne pour la colonne 5
+            DataGridViewTextBoxColumn pourcentageAttenteColumn = new DataGridViewTextBoxColumn();
+            pourcentageAttenteColumn.Name = "Pourcentage Devis Attente";
+            pourcentageAttenteColumn.DataPropertyName = "pourcentaDevisAttente";
+            pourcentageAttenteColumn.HeaderText = "Pourcentage Devis Attente";
+            // Création d'une en-tête de colonne pour la colonne 6
+            DataGridViewTextBoxColumn montantTotalHTColumn = new DataGridViewTextBoxColumn();
+            montantTotalHTColumn.Name = "montant Total facturé HT";
+            montantTotalHTColumn.DataPropertyName = "montantTotalHT";
+            montantTotalHTColumn.HeaderText = "montant Total facturé HT";
+            // Ajout des 6 en-têtes de colonne au datagridview
+            dgvSummary.Columns.Add(nomCliColumn);
+            dgvSummary.Columns.Add(nbDevisColumn);
+            dgvSummary.Columns.Add(nbDevisAcceptésColumn);
+            dgvSummary.Columns.Add(pourcentageAcceptéColumn);
+            dgvSummary.Columns.Add(pourcentageAttenteColumn);
+            dgvSummary.Columns.Add(montantTotalHTColumn);
+
+            //Declation de la liste & injection des valeurs
+
+            dgvSummary.DataSource =
+
+        }
+
+        private void dgvSummary_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
