e3cdb20d594677fdb2eb588176e1683c17447268
unknown <daca.loris@gmail.com>
2022-12-15 09:30:35 +0100

    debut syntheseCli

---
+++ CommercialBO/SyntheseDevis.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommercialBO
+{
+    public class SyntheseDevis
+    {
+        int codeCli;
+        int nbDevis;
+        int nbDevisAcceptes;
+        float pourcentageDevisAcceptes;
+        float pourcentageDevisEnAttente;
+        float montantTotal;
+
+        public SyntheseDevis(int codeCli, int nbDevis, int nbDevisAcceptes, float pourcentageDevisAcceptes, float pourcentageDevisEnAttente, float montantTotal)
+        {
+            this.CodeCli = codeCli;
+            this.NbDevis = nbDevis;
+            this.NbDevisAcceptes = nbDevisAcceptes;
+            this.PourcentageDevisAcceptes = pourcentageDevisAcceptes;
+            this.PourcentageDevisEnAttente = pourcentageDevisEnAttente;
+            this.MontantTotal = montantTotal;
+        }
+
+        public int CodeCli { get => codeCli; set => codeCli = value; }
+        public int NbDevis { get => nbDevis; set => nbDevis = value; }
+        public int NbDevisAcceptes { get => nbDevisAcceptes; set => nbDevisAcceptes = value; }
+        public float PourcentageDevisAcceptes { get => pourcentageDevisAcceptes; set => pourcentageDevisAcceptes = value; }
+        public float PourcentageDevisEnAttente { get => pourcentageDevisEnAttente; set => pourcentageDevisEnAttente = value; }
+        public float MontantTotal { get => montantTotal; set => montantTotal = value; }
+    }
+}
---
+++ CommercialDAL/SyntheseDevisDAO.cs
@@ -0,0 +1,47 @@
+﻿using CommercialBO;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommercialDAL
+{
+    public class SyntheseDevisDAO
+    {
+        public static SyntheseDevis GetSyntheseDevis(int codeCli)
+        {
+            int nomCli;
+            int nbDevis;
+            int nbDevisAcceptes;
+            float pourcentageDevisAcceptes;
+            float pourcentageDevisEnAttente;
+            float montantTotal;
+            // Connexion à la BD
+            SqlConnection maConnexion = ConnexionBD.GetConnexionBD().GetSqlConnexion();
+            // Création d'une liste vide d'objets Produits
+            List<SyntheseDevis> lesSyntheseDevis = new List<SyntheseDevis>();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = maConnexion;
+            cmd.CommandText = "SELECT * FROM Client WHERE code_cli = @codeCli";
+            cmd.Parameters.Add(new SqlParameter("@codeCli", codeCli));
+            SqlDataReader monReader = cmd.ExecuteReader();
+            // Remplissage de la liste
+            while (monReader.Read())
+            {
+                code = Int32.Parse(monReader["code_pro"].ToString());
+                codeCateg = Int32.Parse(monReader["code_categ"].ToString());
+                libelle = monReader["lib_pro"].ToString();
+                float.TryParse(monReader["prix_vente_ht_pro"].ToString(), out prixHT);
+                libelleCategorie = monReader["lib_categ"].ToString();
+                CategorieProduit uneCategPro = new CategorieProduit(codeCateg, libelleCategorie);
+                Produit unProduit = new Produit(code, libelle, prixHT, uneCategPro);
+                lesUtilisateurs.Add(unProduit);
+            }
+            // Fermeture de la connexion
+            maConnexion.Close();
+            return lesUtilisateurs;
+        }
+    }
+}
